65ad20ed4f58cf2cab13f492dd66c327dfa82fb5
Phara0h <t.elsay3d@gmail.com>
2018-10-02 14:08:25 +0900

    some colors and text

--- src/components/Answers/Answers.js
+++ src/components/Answers/Answers.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
+import css from './Answers.css';
 import Answer from './Answer/Answer';
 
 const Answers = props => {
     return (
-        <div className="card__btn-box">
+        <div className={css.card__btn_Box}>
             {props.answers.map((data, i) => (
                 <Answer
                     key={i}
